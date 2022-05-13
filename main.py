01ca6cef9c71a9b0692016411504ab63c63bf37b
Melissa Perez <melmperz@gmail.com>
2022-05-13 18:38:05 -0400

    Add requirements.txt and libraries

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app = Flask(__name__)
