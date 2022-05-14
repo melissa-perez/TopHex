31d7a6406a76f7fb90ccd9e432966318f72b098f
Melissa Perez <melmperz@gmail.com>
2022-05-13 20:26:18 -0400

    Add templates and base functions

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import numpy as np
-from PIL import Image
+import os
+import urllib.request
 import matplotlib.pyplot as plt
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
+from PIL import Image
 
+# PROGRAM CONSTANTS
+IMG_EXTENSION = ('png', 'jpg', 'jpeg', 'gif')
 app = Flask(__name__)
+Bootstrap(app)
+
+
+def valid_image(img_name):
+    """
+    Determines if the img ends with a valid extension.
+    :param img_name: str
+    :return: bool
+    """
+    return False
+
+
+@app.route('/')
+def get_home_page():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    # starts the local host on port 5000
+    app.run(debug=True, host='localhost', port='5000')
