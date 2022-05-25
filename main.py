9bda2cb3f26abb126349d386180ab4ebb912b82a
Melissa Perez <melmperz@gmail.com>
2022-05-24 23:12:32 -0400

    Add primary colors for loaded images

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import urllib
+
 import numpy as np
 import os
 import requests
-import urllib.request
+from PIL.Image import Palette
 from flask import Flask, request, redirect, url_for, render_template
 from PIL import Image
+from colorthief import ColorThief
 from requests import Response
 from werkzeug.utils import secure_filename
 from os.path import join, dirname, realpath
@@ -24,14 +27,12 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 # PROGRAM FUNCTIONS
-
-def create_img_array(np_arr: np.ndarray = None,
-                     from_internet: bool = False,
-                     file_name: str = None,
-                     url: str = None) -> np.ndarray:
+def load_img_to_array(np_arr: np.ndarray = None,
+                      from_internet: bool = False,
+                      file_name: str = None,
+                      url: str = None) -> np.ndarray:
     """
-    Creates an of numeric values representing the
-    image.
+    Loads the image into a numpy array.
 
     :param np_arr: empty object to load data into
     :param from_internet: determines if query is from microservice
@@ -46,7 +47,36 @@ def create_img_array(np_arr: np.ndarray = None,
         load_image_to_array = Image.open(f'{UPLOAD_FOLDER}/{file_name}')
     if not np_arr:
         np_arr = np.array(load_image_to_array, dtype="int32")
+    load_image_to_array.close()
     return np_arr
+
+
+# trying Pithikos SO answer and adapted by me for my program
+def get_dominant_color(img_arr, palette_size=TOP_COLORS):
+    # Resize image to speed up processing
+    print(img_arr)
+    print(type(img_arr))
+    data = Image.fromarray((img_arr * 1).astype(np.uint8)).convert('RGB')
+    print(data)
+    img = data.copy()
+    img.thumbnail((100, 100))
+
+    # Reduce colors (uses k-means internally)
+    paletted = img.convert('P', palette=Palette.ADAPTIVE, colors=palette_size)
+
+    # Find the color that occurs most often
+    palette = paletted.getpalette()
+    color_counts = sorted(paletted.getcolors(), reverse=True)
+    print(color_counts)
+    dominant_colors = []
+    for color in color_counts:
+        print(color)
+        palette_index = color[1]
+        print(palette_index)
+        dominant_colors.append(palette[palette_index * 3:palette_index * 3 + 3])
+
+    print(dominant_colors)
+    return dominant_colors
 
 
 def convert_to_jpg():
@@ -117,7 +147,7 @@ def image_upload():
         response = requests.get(url, params={'url': IMAGE_QUERY_SITE})
 
         img_arr = None
-        img_arr = create_img_array(img_arr, url=response.text, from_internet=True)
+        img_arr = load_img_to_array(img_arr, url=response.text, from_internet=True)
         top_hex_list = top_hex_colors(img_arr)
         return render_template('index.html', filename=response.text, is_upload=False, hex_colors=top_hex_list)
     else:
@@ -128,9 +158,19 @@ def image_upload():
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         img_arr = None
-        img_arr = create_img_array(img_arr, file_name=filename)
+        img_arr = load_img_to_array(img_arr, file_name=filename)
         top_hex_list = top_hex_colors(img_arr)
-        return render_template('index.html', filename=filename, is_upload=True, hex_colors=top_hex_list)
+        top_primary = get_dominant_color(img_arr)
+
+        for i in range(len(top_primary)):
+            top_primary[i] = bytes(tuple(top_primary[i])).hex()
+
+        top_primary = ['#' + code for code in top_primary]
+
+        return render_template('index.html',
+                               filename=filename,
+                               is_upload=True,
+                               colors=zip(top_hex_list,top_primary))
     else:
         return redirect(request.url)
 
